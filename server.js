3ef5e35c35f01b70d7883e570b1614a86c204273
Robbware <robb.alves93@gmail.com>
2019-03-09 23:04:20 +0000

    Removed prompt and added a page with visited count

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 var express = require('express')
 var app = express();
 var path = require('path');
-var prompt = require('prompt')
 
 var totalVisitors = 0;
-
-prompt.start();
-
-prompt.get(['online'], function(err, result) {
-
-    console.log("Users that visited: " + totalVisitors)
-});
-
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/index.html'))
     totalVisitors += 1;
+})
+
+app.get('/visited', function(req, res) {
+    res.send("Visited count: " + totalVisitors);
 })
 
 app.use(express.static(path.join(__dirname, 'public/')));
