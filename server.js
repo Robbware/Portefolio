4c29f6f56ef76b04d34a8dc49db7cd8223466f4c
Robbware <robb.alves93@gmail.com>
2019-03-09 22:31:12 +0000

    Added an express server to host the website on a linux server

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var app = express();
+var path = require('path');
+
+
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname + '/index.html'))
+})
+app.use(express.static(path.join(__dirname, 'public/')));
+app.listen(80, function() {
+    console.log("Listening to server on port 80");
+});
