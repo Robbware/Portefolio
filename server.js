db3feee86620c60ed2c51e7c5cc50da2e5ea8234
Robbware <robb.alves93@gmail.com>
2019-03-09 22:33:48 +0000

    Changed the default port to 8080

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ var path = require('path');
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/index.html'))
+    console.log("New connection found.");
 })
+
 app.use(express.static(path.join(__dirname, 'public/')));
-app.listen(80, function() {
-    console.log("Listening to server on port 80");
+app.listen(8080, function() {
+    console.log("Listening to server on port 8080");
 });
