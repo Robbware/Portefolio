d4ebb02768354556f1c93f4645e29e0ec658febb
Robbware <robb.alves93@gmail.com>
2019-03-10 00:51:16 +0000

    Added facebook visitors count

--- server.js
+++ server.js
@@ -3,14 +3,22 @@ var app = express();
 var path = require('path');
 
 var totalVisitors = 0;
+var facebookVisitors = 0;
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/index.html'))
+    var fbclid = req.query.fbclid;
+
+    if(fbclid) {
+        facebookVisitors += 1;
+    }
+
     totalVisitors += 1;
 })
 
 app.get('/visited', function(req, res) {
     res.send("Visited count: " + totalVisitors);
+    res.send("From Facebook: " + facebookVisitors);
 })
 
 app.use(express.static(path.join(__dirname, 'public/')));
